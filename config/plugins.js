1e84ccd57f9c8a66351274204893e4a0b66b5291
amoghshetty18 <virkohli30@gmail.com>
2022-05-21 19:16:21 +0530

    Initial Commit

---
+++ config/plugins.js
@@ -0,0 +1,27 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        uploadStream: {
+          folder: env("CLOUDINARY_FOLDER"),
+        },
+        delete: {},
+      },
+      breakpoints: {
+        // xlarge: 1920,
+        // large: 1000,
+        // medium: 750,
+        // small: 500,
+        // xsmall: 64
+      },
+    },
+  },
+  // ...
+});
